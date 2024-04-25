40e8452f7785b8af203acbe374df757ec34a65ce
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2024-04-25 10:54:38 +0400

    json stringify

--- client/index.js
+++ client/index.js
@@ -2,9 +2,11 @@ const url = "ws://178.208.81.203:9000";
 const websocket = new WebSocket(url);
 
 websocket.addEventListener("open", () => {
-  websocket.send({
-    message: "Hello server!",
-    data: ["1", "2", "3", "4"],
-    raw: { name: "alex" },
-  });
+  websocket.send(
+    JSON.stringify({
+      message: "Hello server!",
+      data: ["1", "2", "3", "4"],
+      raw: { name: "alex" },
+    }),
+  );
 });
