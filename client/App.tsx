a4e4fa514133e06ad702cfc2127ac435bc7d3d79
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2024-04-25 20:21:32 +0400

    messages

--- client/App.tsx
+++ client/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
+type TMessage = {
+  name: string;
+  message: string;
+};
 
 export const App = () => {
   const url = `ws://${process.env.URL}`;
-  const [message, setMessage] = useState("");
+  const [name, setName] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [isChat, setIsChat] = useState<boolean>(false);
+  const [websoket, setWebsoket] = useState<WebSocket | null>(null);
+  const [serverMessages, setServerMessages] = useState<TMessage[]>([]);
 
   function connectToChat() {
     const websocket = new WebSocket(url);
     websocket.addEventListener("open", () => {
-      websocket.send(
-        JSON.stringify({
-          message: "Hello server!",
-          data: ["1", "2", "3", "4"],
-          raw: { name: "alex" },
-        }),
-      );
+      alert("Connected to the chat!");
     });
     websocket.addEventListener("message", (message) => {
       const msg = JSON.parse(message.data);
       console.log(msg);
     });
+    setWebsoket(websocket);
+    setIsChat(true);
   }
 
   function onInput(e: React.ChangeEvent) {
     setMessage(e.target.nodeValue);
   }
 
+  function assignName(e: React.ChangeEvent) {
+    setName(e.target.nodeValue);
+  }
+
+  function sendMessage() {
+    websoket.send(
+      JSON.stringify({
+        name,
+        message,
+      }),
+    );
+  }
+
   return (
     <>
       <h1>Welcome to the chat</h1>
-      <input type="text" value={message} onChange={onInput} />
-      <button onClick={connectToChat}>Start</button>
+      {isChat ? (
+        <>
+          <input
+            type="text"
+            value={message}
+            onChange={onInput}
+            placeholder="Enter a message"
+          />
+          <button onClick={sendMessage}>Send message</button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            value={name}
+            onChange={assignName}
+            placeholder="Enter your name"
+          />
+          <button onClick={connectToChat}>Start</button>
+        </>
+      )}
+      {serverMessages.length ? (
+        serverMessages.map((message) => (
+          <p>{message.name + ": " + message.message}</p>
+        ))
+      ) : (
+        <h2>No new messages!</h2>
+      )}
     </>
   );
 };
