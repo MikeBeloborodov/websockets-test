b1bb3b10ab3ac99ff65a106cd67d63ada6a3e2bd
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2024-04-25 19:58:03 +0400

    ts server

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const WebSocket = require("ws");
-
-const wsServer = new WebSocket.Server({ port: 9000 });
-
-wsServer.on("connection", (wsClient) => {
-  console.log("New user");
-  wsClient.send("Hello!");
-  wsClient.on("message", (message) => {
-    const msg = JSON.parse(message);
-    console.log(msg);
-  });
-  wsClient.on("close", () => {
-    console.log("User logout");
-  });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ws = require("ws");
+var wsServer = new ws.Server({ port: 9000 });
+wsServer.on("connection", function (wsClient) {
+    console.log("New user");
+    wsClient.send("Hello!");
+    wsClient.on("message", function (message) {
+        var msg = JSON.parse(message);
+        console.log(msg);
+        wsServer.clients.forEach(function (client) {
+            client.send(message);
+        });
+    });
+    wsClient.on("close", function () {
+        console.log("User logout");
+    });
 });
-
 console.log("Server is working on port 9000");
---
+++ index.ts
@@ -0,0 +1,29 @@
+import * as ws from "ws";
+
+const wsServer = new ws.Server({ port: 9000 });
+
+type TMessage = {
+  name: string;
+  message: string;
+};
+
+wsServer.on("connection", (wsClient) => {
+  console.log("New user");
+  wsClient.send("Hello!");
+
+  wsClient.on("message", (message: string) => {
+    const msg: TMessage = JSON.parse(message);
+
+    console.log(msg);
+
+    wsServer.clients.forEach((client) => {
+      client.send(message);
+    });
+  });
+
+  wsClient.on("close", () => {
+    console.log("User logout");
+  });
+});
+
+console.log("Server is working on port 9000");
