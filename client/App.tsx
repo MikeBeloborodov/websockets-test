ea60bf9330a75ee66fcf248a2061e1dadccff40e
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2024-04-25 17:05:18 +0400

    chat

--- client/App.tsx
+++ client/App.tsx
@@ -1,16 +1,26 @@
 export const App = () => {
   const url = `ws://${process.env.URL}`;
-  const websocket = new WebSocket(url);
 
-  websocket.addEventListener("open", () => {
-    websocket.send(
-      JSON.stringify({
-        message: "Hello server!",
-        data: ["1", "2", "3", "4"],
-        raw: { name: "alex" },
-      }),
-    );
-  });
+  function connectToChat() {
+    const websocket = new WebSocket(url);
+    websocket.addEventListener("open", () => {
+      websocket.send(
+        JSON.stringify({
+          message: "Hello server!",
+          data: ["1", "2", "3", "4"],
+          raw: { name: "alex" },
+        }),
+      );
+    });
+    websocket.addEventListener("message", (message) => {
+      console.log(message);
+    });
+  }
 
-  return <h1>Welcome to the chat</h1>;
+  return (
+    <>
+      <h1>Welcome to the chat</h1>
+      <button onClick={connectToChat}>Start</button>
+    </>
+  );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ module.exports = {
     },
     compress: true,
     port: 4000,
+    allowedHosts: "all",
   },
   mode: "production",
 };
