3d8828d90e9f21e69c739bab09d8c035be93dee6
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2024-04-25 10:50:50 +0400

    fix1

--- client/index.js
+++ client/index.js
@@ -1,10 +1,6 @@
-const url = "wss://178.208.81.203:9000";
+const url = "ws://178.208.81.203:9000";
 const websocket = new WebSocket(url);
-
-console.log(websocket.readyState);
 
 websocket.addEventListener("open", () => {
   websocket.send("say hi to server!");
 });
-
-websocket.close(1000, "connection closed!");
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const wsServer = new WebSocket.Server({ port: 9000 });
 
 wsServer.on("connection", (wsClient) => {
   console.log("New user");
-  console.log(wsClient);
   wsClient.send("Hello!");
   wsClient.on("message", (message) => {
     console.log(message);
